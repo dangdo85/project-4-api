e2361baddb00d9866cbf41a6eb6ef3d0235b7344
tinhi3 <lancetnguyen100@gmail.com>
2022-08-19 23:19:47 -0400

    setting up routes for plant and myplant

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const plantRoutes = require('./app/routes/plant_routes')
+const myplantRoutes = require('./app/routes/myplant_routes')
 const userRoutes = require('./app/routes/user_routes')
+const exampleRoutes = require('./app/routes/example_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,8 +67,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(plantRoutes)
+app.use(myplantRoutes)
 app.use(userRoutes)
+app.use(exampleRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
